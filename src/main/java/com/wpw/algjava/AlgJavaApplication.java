cade035285c40d8eee031e70815c55cff2e3332c
wangpengwei <wangpengweiWw@163.com>
2020-06-14 18:24:36 +0800

    提交自己写的剑指offer-java版本的40题题解和思路

---
+++ src/main/java/com/wpw/algjava/AlgJavaApplication.java
@@ -0,0 +1,13 @@
+package com.wpw.algjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wpw/algjava/AlgJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wpw.algjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
